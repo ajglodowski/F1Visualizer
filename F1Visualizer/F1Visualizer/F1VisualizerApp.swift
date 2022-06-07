ab279ac10a13e463e9ea2c7a1fb3c973bbb0c028
AJ Glodowski <ajglodo@gmail.com>
2022-06-06 21:56:15 -0400

    Project Start

---
+++ F1Visualizer/F1Visualizer/F1VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1VisualizerApp.swift
+//  F1Visualizer
+//
+//  Created by AJ Glodowski on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct F1VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
